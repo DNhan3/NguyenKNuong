d26bbbe758e562d8db6a846682d56c85e37ed858
vvkquau <ducnhan141106@gmail.com>
2023-09-06 23:22:58 +0700

    done

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-function phone() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("0937840446");
-
-    /* Alert the copied text */
-    alert("You have copied my phone  number !");
+function show() {
+    document.getElementById('close')
+        .style.display = "block";
+    document.getElementById('popcover')
+        .style.display = "block";
 }
 
-function email() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("ducnhan141106@gmail.com");
-
-    /* Alert the copied text */
-    alert("You have copied my email address !");
-}
-
-function bank() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("1034561329");
-
-    /* Alert the copied text */
-    alert("You have copied my bank number account ✨");
+function hide() {
+    document.getElementById('popcover')
+        .style.display = "none";
+    document.getElementById('close')
+        .style.display = "none";
 }

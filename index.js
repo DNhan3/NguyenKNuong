ce96519b2e853ec4aaa54dca861712f84b7e6f08
vvkquau <ducnhan141106@gmail.com>
2023-09-19 14:43:22 +0700

    done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function show() {
-    document.getElementById('close')
-        .style.display = "block";
-    document.getElementById('popcover')
-        .style.display = "block";
+    document.getElementById('close').style.display = "block";
+    document.getElementById('popcover').style.display = "block";
+    document.getElementById('close').style.position = "fixed";
+    document.getElementById('popcover').style.position = "fixed";
 }
 
 function hide() {
@@ -10,4 +14,16 @@ function hide() {
         .style.display = "none";
     document.getElementById('close')
         .style.display = "none";
+}
+
+function docshow(){
+    document.getElementById('part1').style.animation = "fadeout 2s linear forwards";
+    document.getElementById('part2').style.display = "block";
+    document.getElementById('part2').style.animation = "faded 2s 2s linear forwards ";
+}
+
+function dochidden(){
+    document.getElementById('part2').style.display = "none";
+    document.getElementById('part1').style.display = "block";
+    document.getElementById('part1').style.animation = "fade 2s linear forwards";
 }
